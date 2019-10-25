b5c9468dbeeffdae40b14ced5ef63a2be68a51ac
alyssalanae <briggs.l.alyssa@gmail.com>
2019-10-25 11:20:19 -0500

    fixed tabs

--- main.js
+++ main.js
@@ -1,95 +1,5 @@
-"use strict"
+"use strict";
 
-function renderCoffee(coffee) {
-    var html = '';
-    html +=  coffee.name + " " ;
-    html += '<p class="card-text"><small class="text-muted">' + coffee.roast + '</small></p>';
-
-    return html;
-}
-
-function renderCoffeeLight(coffee) {
-    var html = '';
-    html +=  coffee.name + " " ;
-    html += '<p class="card-text"><small class="text-muted">' + coffee.roast + '</small></p>';
-if (coffee.roast === "light") {
-    return html;
-}
-}
-
-function renderCoffeeMedium(coffee) {
-    var html = '';
-    html +=  coffee.name + " " ;
-    html += '<p class="card-text"><small class="text-muted">' + coffee.roast + '</small></p>';
-    if (coffee.roast === "medium") {
-        return html;
-    }
-}
-
-function renderCoffees(coffees) {
-    var html = '<div class="card-columns-3">';
-    for(var i = 0; i < coffees.length; i++) {
-        html += '<div class="card text-center">' +
-            '<div class="card-body">' +
-            '<h5 class="card-title">' + renderCoffee(coffees[i]) + '</h5>'+
-            '</div>' +
-            '</div>'
-    }
-    return html;
-}
-
-function renderCoffeesLight(coffees) {
-    var html = '<div class="card-columns-3">';
-    for(var i = 0; i < coffees.length; i++) {
-        html += '<div class="card text-center">' +
-            '<div class="card-body">' +
-            '<h5 class="card-title">' + renderCoffeeLight(coffees[i]) + '</h5>'+
-            '</div>' +
-            '</div>'
-    }
-    return html;
-}
-
-function renderCoffeesMedium(coffees) {
-    var html = '<div class="card-columns-3">';
-    for(var i = 0; i < coffees.length; i++) {
-        html += '<div class="card text-center">' +
-            '<div class="card-body">' +
-            '<h5 class="card-title">' + renderCoffeeMedium(coffees[i]) + '</h5>'+
-            '</div>' +
-            '</div>'
-    }
-    return html;
-}
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
-}
-
-function chooseCoffee(){
-    var html = '<div class="card-columns-3">';
-    for(var i = 0; i < coffees.length; i++) {
-        if (coffees[i].name.toLowerCase().includes(coffeeName.value.toLowerCase())) {
-            html += '<div class="card text-center">' +
-                '<div class="card-body">' +
-                '<h5 class="card-title">' + renderCoffee(coffees[i]) + '</h5>'+
-                '</div>' +
-                '</div>'
-        }
-        tbody.innerHTML = html;
-    }
-}
-document.getElementById("coffeeName").addEventListener("keyup", chooseCoffee);
-
-
-// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
     {id: 2, name: 'Half City', roast: 'light'},
@@ -107,17 +17,100 @@ var coffees = [
     {id: 14, name: 'French', roast: 'dark'},
 ];
 
+function renderCoffee(coffee) {
+    var html = '';
+    html +=  coffee.name + " " ;
+    html += '<p class="card-text"><small class="text-muted">' + coffee.roast + '</small></p>';
+
+    return html;
+}
+
+function renderCoffees(coffees) {
+    var html = '<div class="card-columns-3">';
+    for(var i = 0; i < coffees.length; i++) {
+        html += '<div class="card text-center">' +
+            '<div class="card-body">' +
+            '<h5 class="card-title">' + renderCoffee(coffees[i]) + '</h5>'+
+            '</div>' +
+            '</div>'
+    }
+    return html;
+}
+
+function updateCoffees(e) {
+    e.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection.value;
+    console.log(selectedRoast);
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    tbody.innerHTML = renderCoffees(filteredCoffees);
+}
+
+function updateCoffeesTabs(event, roastSelection) {
+    console.log(event);
+    event.preventDefault(); // don't submit the form, we just want to update the data
+    var selectedRoast = roastSelection;
+    console.log(selectedRoast);
+    var filteredCoffees = [];
+    coffees.forEach(function(coffee) {
+        if (coffee.roast === selectedRoast) {
+            filteredCoffees.push(coffee);
+        }
+    });
+    console.log(filteredCoffees);
+    return renderCoffees(filteredCoffees);
+}
+
+function chooseCoffee(){
+    var html = '<div class="card-columns-3">';
+    for(var i = 0; i < coffees.length; i++) {
+        if (coffees[i].name.toLowerCase().includes(coffeeName.value.toLowerCase())) {
+            html += '<div class="card text-center">' +
+                '<div class="card-body">' +
+                '<h5 class="card-title">' + renderCoffee(coffees[i]) + '</h5>'+
+                '</div>' +
+                '</div>'
+        }
+        tbody.innerHTML = html;
+    }
+}
+document.getElementById("coffeeName").addEventListener("keyup", chooseCoffee);
+
+document.getElementById("nav-light-tab").addEventListener("click", function(event){
+    console.log("in light click");
+    lbody.innerHTML = updateCoffeesTabs(event, "light");
+    mbody.innerHTML = "";
+    dbody.innerHTML = "";
+});
+document.getElementById("nav-medium-tab").addEventListener("click", function(event){
+    console.log("in medium click");
+    lbody.innerHTML = "";
+    dbody.innerHTML = "";
+    mbody.innerHTML = updateCoffeesTabs(event, "medium");
+});
+document.getElementById("nav-dark-tab").addEventListener("click", function(event){
+    console.log("in dark click");
+    dbody.innerHTML = updateCoffeesTabs(event, "dark");
+    mbody.innerHTML = "";
+    lbody.innerHTML = "";
+});
+
+// from http://www.ncausa.org/About-Coffee/Coffee-Roasts-Guide
+
 
 
 var tbody = document.querySelector('#coffees');
 var lbody = document.querySelector('#coffees-light');
 var mbody = document.querySelector('#coffees-medium');
+var dbody = document.querySelector('#coffees-dark');
 var submitButton = document.querySelector('#submit');
 var roastSelection = document.querySelector('#roast-selection');
 
 tbody.innerHTML = renderCoffees(coffees);
-lbody.innerHTML = renderCoffeesLight(coffees);
-mbody.innerHTML = renderCoffeesMedium(coffees);
 
 
 submitButton.addEventListener('click', updateCoffees);
